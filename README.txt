eb147c4f5580d309179d5ded217a37cefddeff67
Tim Fletcher <mail@tfletcher.com>
2013-02-27 12:34:05 +0000

    Add second argument for variable lookup

--- README.txt
+++ README.txt
@@ -12,9 +12,13 @@ Quick start:
   <<"<h1>hello world</h1>">>
   3> iolist_to_binary(html:render({input, [{type, text}, {size, 30}]})).
   <<"<input type=\"text\" size=\"30\">">>
+  4> iolist_to_binary(html:render([<<"hi ">>, user.name], [{user.name, <<"joe">>}])).
+  <<"hi joe">>
   ...
 
 Pass terms to html:render/1 and format the result with iolist_to_binary/1.
+
+Use html:render/2 with an additional proplist to use for variable lookup.
 
 Here's a more complete example:
 
--- src/html.erl
+++ src/html.erl
@@ -1,6 +1,6 @@
 -module(html).
 
--export([render/1]).
+-export([render/1, render/2]).
 
 -compile({inline, [{start_tag, 2}, {end_tag, 1}, {escapes, 0}, {space, 0}]}).
 
@@ -19,26 +19,35 @@ render_test_() -> [
   , ?_assertEqual(<<"<h1>1234567890</h1>">>, iolist_to_binary(render({h1, [], 1234567890})))
   , ?_assertEqual(<<"<ul><li>1</li><li>2</li></ul>">>, iolist_to_binary(render({ul, [], [{li, [], 1}, {li, [], 2}]})))
   , ?_assertEqual(<<"<h1><span>hello world</span></h1>">>, iolist_to_binary(render({h1, {span, <<"hello world">>}})))
+  , ?_assertEqual(<<"<span>42</span>">>, iolist_to_binary(render({span, [], 'X'}, [{'X', 42}])))
   ].
 
-render({Name}) when is_atom(Name) ->
+render(Term) ->
+  render(Term, []).
+
+render({Name}, _Context) when is_atom(Name) ->
   start_tag(Name, []);
-render({Name, AttrList, Content}) ->
-  [start_tag(Name, AttrList), render(Content), end_tag(Name)];
-render({Name, Content}) when is_atom(Name), is_binary(Content) ->
-  [start_tag(Name, []), render(Content), end_tag(Name)];
-render({Name, Content}) when is_atom(Name), is_tuple(Content) ->
-  [start_tag(Name, []), render(Content), end_tag(Name)];
-render({Name, AttrList}) when is_atom(Name) ->
+render({Name, AttrList, Content}, Context) ->
+  [start_tag(Name, AttrList), render(Content, Context), end_tag(Name)];
+render({Name, Content}, Context) when is_atom(Name), is_binary(Content) ->
+  [start_tag(Name, []), render(Content, Context), end_tag(Name)];
+render({Name, Content}, Context) when is_atom(Name), is_tuple(Content) ->
+  [start_tag(Name, []), render(Content, Context), end_tag(Name)];
+render({Name, AttrList}, _Context) when is_atom(Name) ->
   start_tag(Name, AttrList);
-render(Input) when is_list(Input) ->
-  [render(Term) || Term <- Input];
-render(Input) when is_integer(Input) ->
+render(Input, Context) when is_list(Input) ->
+  [render(Term, Context) || Term <- Input];
+render(Input, _Context) when is_integer(Input) ->
   integer_to_list(Input);
-render(Input) when is_atom(Input) ->
-  atom_to_list(Input);
-render(Input) when is_binary(Input) ->
-  escape(Input).
+render(Input, _Context) when is_binary(Input) ->
+  escape(Input);
+render(Input, Context) when is_atom(Input) ->
+  case proplists:lookup(Input, Context) of
+    {_, Term} ->
+      render(Term, Context);
+    none ->
+      atom_to_list(Input)
+  end.
 
 attr_render({Name, Value}) when is_atom(Name) ->
   [space(), atom_to_list(Name), $=, $", attr_value_render(Value), $"];
